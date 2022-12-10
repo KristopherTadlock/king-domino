edc78b4bb201da2fec491bc29cb481df169d9b49
Roman Pshichenko <Pshichenko@hotmail.com>
2022-12-10 13:29:42 -0800

    changing project to use module | updateing package to run tests

--- test/test-runner.js
+++ test/test-runner.js
@@ -1,5 +1,5 @@
 //import * as testExample from '/test/test-example.js';
-import * as testDominoTile from '/test/test-domino-tile.js';
-import * as testDomino from '/test/test-domino.js';
-import * as testBoardMinMaxAxis from '/test/test-board-min-max-axis.js';
-import * as testGameBoardManager from '/test/test-game-board-manager.js';
+import * as testDominoTile from './test-domino-tile.js';
+import * as testDomino from './test-domino.js';
+import * as testBoardMinMaxAxis from './test-board-min-max-axis.js';
+import * as testGameBoardManager from './test-game-board-manager.js';
