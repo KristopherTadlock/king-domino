4ac4fd60475673bac7b4a286c8b679cc6882829e
Roman Pshichenko <Pshichenko@hotmail.com>
2022-12-10 13:34:53 -0800

    all failing tests should b logged at the end of successful tests

--- test/test-framework.js
+++ test/test-framework.js
@@ -6,11 +6,15 @@ export const it = (desc, func) => {
     // If the test case passes then log the test case description in the browser console with a checkmark
     console.log("\x1b[32m%s\x1b[0m", "\u2714 " + desc);
   } catch (error) {
-    // log the error on the console with an 'x'
-    console.log("\n");
-    console.log("\x1b[31m%s\x1b[0m", "\u2718 " + desc);
-    console.error(error);
-    console.log("\n");
+    // If the test case fails then log the test case description in the browser console with an 'x'
+    // wrap in setTimeout so that the error is logged after the test all succcessful tests
+    setTimeout(() => {
+      // log the error on the console with an 'x'
+      console.log("\n");
+      console.log("\x1b[31m%s\x1b[0m", "\u2718 " + desc);
+      console.error(error);
+      console.log("\n");
+    }, 0);
   }
 };
 
